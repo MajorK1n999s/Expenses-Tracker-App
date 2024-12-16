fb5262d6c9f32e32072f91531f6235fa85a1a2b9
Mahebubbasha Saache <mahebubbashasaache@gmail.com>
2024-12-16 11:21:39 +0530

    Initial commit

---
+++ Expense Tracker/Program.cs
@@ -0,0 +1,39 @@
+using Expense_Tracker.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Register Syncfusion license
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NMaF5cXmBCf1FpR2JGfV5ycEVHal9TTnJZUiweQnxTdEFiW35acHVWRGRaVUJ+Xg==");
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Category}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,2 @@
+# Expenses-Tracker-App
+ it is made in asp.net core mvc using c# for tracking your daily or monthly expenses with better UI represtation
